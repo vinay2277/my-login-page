42443475039748659d853dbe25fd88119b2698dd
vinay2277 <111374885+vinay2277@users.noreply.github.com>
2023-08-09 13:32:01 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+	$username= $_POST['username'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','test');
+	if($mysqli-> connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(username,  email, password) values(?, ?, ?)");
+		$stmt->bind_param("sss", $username,  $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
